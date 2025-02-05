5ffae27386fa3cd6fd06b28782d8f211d4bfc79f
Rick <26606521+rycalvo@users.noreply.github.com>
2025-02-05 11:07:42 -0800

    Create week1_exercise.R

---
+++ assignments/week1_exercise.R
@@ -0,0 +1,17 @@
+# PH700A Week 1
+# In Class Exercise
+###################
+
+# Create objects called "firstname" and "lastname" that are your first name and last name, respectively.
+
+# Create a vector that holds values, 12, 24, 48, 168
+
+# Calculate how many hours are in 31 days and store it in a object
+
+# Add the new object to the original vector
+
+# Create a new vector for label names for the first vector and apply them to the values in the vector
+
+# Calculate the percent of the total time (max time = 31 days) for each part of the initial vector.
+
+# Output the final vector
